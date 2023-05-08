a2b4067c8ed676191e97ea50898c6f57a5781328
Enos Namiinda <namiindaenos@gmail.com>
2023-05-08 15:21:29 -0700

    corrections

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-
-str = (str[39:66] + str[106:112] + str[0:6])
-print(str)
+str = "Python is an interpreted, interactive, object-oriented programming\ language that combines remarkable power with very clear syntax"
+print(str.split(" ")[7] + " " + str.split(" ")[8] + " " + str.split(" ")[9] + " " + str.split(" ")[10] + " " + str.split(" "[0] + "\n")
